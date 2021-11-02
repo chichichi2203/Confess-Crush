19a9ea687e4f17d093dd0da8c4865ab32f870950
chichichi2203 <93566378+chichichi2203@users.noreply.github.com>
2021-11-02 11:17:34 +0700

    Update config.js

--- config.js
+++ config.js
@@ -9,10 +9,10 @@ const CONFIG = {
     btnYes: 'Yêu em',
     btnNo: 'Không nha :3',
     question: 'Thế giới này nhiều người như thế tại sao anh lại yêu em nhỉ <3',
-    btnReply: 'Em bảo này <3',
-    reply: 'DOVE-Do yOu loVe me? Anh có yêu em không <33333333',
+    btnReply: 'Em bảo nè <3',
+    reply: 'Vì em xinh này, ngoan này, dễ thương này <33333333',
     mess: 'Trả lời em',
-    messDesc: 'Lúc nào cũng được, hãy nhắn tin cho em khi anh rảnh nhé, bất cứ khi nào',
+    messDesc: 'Hãy nhắn tin cho em khi anh thấy cái này nhé',
     btnAccept: 'Yêu em <3',
     messLink: 'https://www.messenger.com/t/100052824681391' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
